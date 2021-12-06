68af6bccd34ebaad57b1cea2b896da83276c79a3
Eric Brelsford <ebrelsford@gmail.com>
2021-12-05 23:12:47 -0500

    Start to organize linechart generator

--- src/LinesChart.svelte
+++ src/LinesChart.svelte
@@ -27,6 +27,197 @@
 
   let layers;
 
+  function getColor(layer, xScale, yScale) {
+    const lineStart = xScale(layer.minzoom || minZoom);
+    const lineLength = xScale(layer.maxzoom || maxZoom) - xScale(layer.minzoom ||
+      minZoom);
+
+    let color;
+    let opacity;
+    let strokeColor;
+    let strokeWidth = 0;
+    switch (layer.type) {
+      case 'fill': 
+        color = layer.paint['fill-color'];
+        opacity = layer.paint['fill-opacity'];
+        strokeColor = layer.paint['fill-outline-color'];
+        if (strokeColor) strokeWidth = 1;
+        break;
+      case 'background':
+        color = layer.paint['background-color'];
+        break;
+      case 'line':
+        color = layer.paint['line-color'];
+        opacity = layer.paint['line-opacity'];
+        break;
+      case 'symbol':
+        color = layer.paint ? layer.paint['text-color'] : 'black';
+        opacity = layer.paint ? layer.paint['text-opacity'] : null;
+        break;
+      default: {}
+    }
+
+    let colorIsGradient = false;
+    let opacityIsGradient = false;
+
+    if (Array.isArray(color)) {
+      const [expressionType, [interpolationType], [attribute]] = color;
+
+      if (expressionType === 'interpolate' && interpolationType === 'linear' && attribute === 'zoom') {
+        colorIsGradient = true;
+      }
+    }
+
+    if (Array.isArray(opacity)) {
+      const [expressionType, [interpolationType], [attribute]] = opacity;
+
+      if (expressionType === 'interpolate' && interpolationType === 'linear' && attribute === 'zoom') {
+        opacityIsGradient = true;
+      }
+    }
+
+    const gradientStops = [];
+
+    if (colorIsGradient) {
+      const gradientArray = color.slice(3);
+
+      for (let i = 0; i < gradientArray.length; i += 2) {
+        const offset = ((xScale(gradientArray[i]) - lineStart) / lineLength) * 100;
+
+        gradientStops.push({
+          offset,
+          stopColor: gradientArray[i + 1],
+          stopOpacity: 1
+        });
+      }
+    }
+
+    if (opacityIsGradient && !colorIsGradient) {
+      const gradientArray = opacity.slice(3);
+
+      for (let i = 0; i < gradientArray.length; i += 2) {
+        const offset = ((xScale(gradientArray[i]) - lineStart) / lineLength) * 100;
+        gradientStops.push({
+          offset,
+          stopColor: color,
+          stopOpacity: gradientArray[i + 1]
+        });
+      }
+    }
+
+    if (colorIsGradient && opacityIsGradient) {
+      // TODO handle these cases
+      console.log(layer.id, 'both color and opacity are gradients');
+    }
+
+    if (gradientStops.length > 1) {
+      gradients = [
+        ...gradients,
+        {
+          id: layer.id,
+          stops: gradientStops.map(stop => ({
+            offset: `${stop.offset}%`,
+            stopColor: stop.stopColor,
+            stopOpacity: stop.stopOpacity,
+          }))
+        }
+      ];
+
+      color = `url('#${layer.id}')`;
+    }
+
+    return { color, strokeColor, strokeWidth };
+  }
+
+  function getPath(layer, xScale, yScale) {
+    const width = layer.paint ? layer.paint['line-width'] : null;
+    const layerMaxZoom = layer.maxzoom || maxZoom;
+    const layerMinZoom = layer.minzoom !== undefined ? layer.minzoom : minZoom;
+    const topPoints = [];
+    const bottomPoints = [];
+    let lastZoom;
+    let lastWidth;
+    let path;
+
+    if (Array.isArray(width)) {
+      const [expressionType, [interpolationType, interpolationBase], [attribute]] = width;
+      if (expressionType === 'interpolate' && interpolationType === 'linear' && attribute === 'zoom') {
+        // TODO look into base for interpolation
+
+        const widthArray = width.slice(3);
+        topPoints.push([
+          xScale(layerMinZoom),
+          yScale(layer.id) + (yScale.bandwidth() / 2)
+        ]);
+        for (let i = 0; i < widthArray.length; i += 2) {
+          const zoom = widthArray[i];
+          const width = widthArray[i + 1];
+
+          if (zoom < layerMinZoom) continue;
+
+          topPoints.push([xScale(zoom), yScale(layer.id) - (width / 2) + yScale.bandwidth() / 2]);
+          if (width > 0) {
+            bottomPoints.push([xScale(zoom), yScale(layer.id) + (width / 2) + yScale.bandwidth() / 2]);
+          }
+        }
+
+        [lastZoom, lastWidth] = widthArray.slice(-2);
+      }
+      else if (interpolationType === 'exponential') {
+        // TODO create scale, sample zoom levels, create points
+      }
+      else {
+        console.log('array but not linear', width);
+      }
+    }
+    else if (width && width.stops) {
+      width.stops.forEach(stop => {
+        const [zoom, width] = stop;
+
+        topPoints.push([xScale(zoom), yScale(layer.id) - (width / 2) + yScale.bandwidth() / 2]);
+        if (width > 0) {
+          bottomPoints.push([xScale(zoom), yScale(layer.id) + (width / 2) + yScale.bandwidth() / 2]);
+        }
+      });
+
+      [lastZoom, lastWidth] = width.stops.slice(-1)[0];
+    }
+    else if (!width) {
+      let layerWidth = 1;
+      topPoints.push([xScale(layerMinZoom), yScale(layer.id) - (layerWidth / 2) + yScale.bandwidth() / 2]);
+      topPoints.push([xScale(layerMaxZoom), yScale(layer.id) - (layerWidth / 2) + yScale.bandwidth() / 2]);
+      bottomPoints.push([xScale(layerMinZoom), yScale(layer.id) + (layerWidth / 2) + yScale.bandwidth() / 2]);
+      bottomPoints.push([xScale(layerMaxZoom), yScale(layer.id) + (layerWidth / 2) + yScale.bandwidth() / 2]);
+    }
+
+    if (topPoints.length) {
+      if (layerMaxZoom > lastZoom) {
+        topPoints.push([xScale(layerMaxZoom), yScale(layer.id) - (lastWidth / 2) + yScale.bandwidth() / 2]);
+        if (lastWidth > 0) {
+          bottomPoints.push([xScale(layerMaxZoom), yScale(layer.id) + (lastWidth / 2) + yScale.bandwidth() / 2]);
+        }
+      }
+
+      path = topPoints.concat(bottomPoints.reverse());
+    }
+
+    return path;
+  }
+
+  function getDrawLayer(layer, xScale, yScale) {
+
+    const { color, strokeColor, strokeWidth } = getColor(layer, xScale, yScale);
+    const path = getPath(layer, xScale, yScale);
+
+    return {
+      ...layer,
+      path: d3.line()(path || []),
+      fill: color,
+      stroke: strokeColor,
+      strokeWidth: strokeWidth,
+    };
+  }
+
   $: if (style && style.layers) {
     const lineLayers = style.layers.filter(l => l.type === 'line');
     height = style.layers.length * 65;
@@ -37,187 +228,7 @@
 
     zoomLevels = d3.range(minZoom, maxZoom + 1, 1);
 
-    layers = lineLayers.map(l => {
-      const lineStart = xScale(l.minzoom || minZoom);
-      const lineLength = xScale(l.maxzoom || maxZoom) - xScale(l.minzoom ||
-        minZoom);
-
-      let color;
-      let opacity;
-      let strokeColor;
-      let strokeWidth = 0;
-      switch (l.type) {
-        case 'fill': 
-          color = l.paint['fill-color'];
-          opacity = l.paint['fill-opacity'];
-          strokeColor = l.paint['fill-outline-color'];
-          if (strokeColor) strokeWidth = 1;
-          break;
-        case 'background':
-          color = l.paint['background-color'];
-          break;
-        case 'line':
-          color = l.paint['line-color'];
-          opacity = l.paint['line-opacity'];
-          break;
-        case 'symbol':
-          color = l.paint ? l.paint['text-color'] : 'black';
-          opacity = l.paint ? l.paint['text-opacity'] : null;
-          break;
-        default: {}
-      }
-
-      let colorIsGradient = false;
-      let opacityIsGradient = false;
-
-      if (Array.isArray(color)) {
-        const [expressionType, [interpolationType], [attribute]] = color;
-
-        if (expressionType === 'interpolate' && interpolationType === 'linear' && attribute === 'zoom') {
-          colorIsGradient = true;
-        }
-      }
-
-      if (Array.isArray(opacity)) {
-        const [expressionType, [interpolationType], [attribute]] = opacity;
-
-        if (expressionType === 'interpolate' && interpolationType === 'linear' && attribute === 'zoom') {
-          opacityIsGradient = true;
-        }
-      }
-
-      const gradientStops = [];
-
-      if (colorIsGradient) {
-        const gradientArray = color.slice(3);
-        if (l.id === 'road_motorway-casing') {
-          console.log(gradientArray, l.minzoom);
-        }
-
-        for (let i = 0; i < gradientArray.length; i += 2) {
-          const offset = ((xScale(gradientArray[i]) - lineStart) / lineLength) * 100;
-
-          gradientStops.push({
-            offset,
-            stopColor: gradientArray[i + 1],
-            stopOpacity: 1
-          });
-        }
-      }
-
-      if (opacityIsGradient && !colorIsGradient) {
-        const gradientArray = opacity.slice(3);
-
-        for (let i = 0; i < gradientArray.length; i += 2) {
-          const offset = ((xScale(gradientArray[i]) - lineStart) / lineLength) * 100;
-          gradientStops.push({
-            offset,
-            stopColor: color,
-            stopOpacity: gradientArray[i + 1]
-          });
-        }
-      }
-
-      if (colorIsGradient && opacityIsGradient) {
-        // TODO handle these cases
-        console.log(l.id, 'both color and opacity are gradients');
-      }
-
-      if (gradientStops.length > 1) {
-        gradients = [
-          ...gradients,
-          {
-            id: l.id,
-            stops: gradientStops.map(stop => ({
-              offset: `${stop.offset}%`,
-              stopColor: stop.stopColor,
-              stopOpacity: stop.stopOpacity,
-            }))
-          }
-        ];
-
-        color = `url('#${l.id}')`;
-      }
-
-      const width = l.paint ? l.paint['line-width'] : null;
-      const layerMaxZoom = l.maxzoom || maxZoom;
-      const layerMinZoom = l.minzoom !== undefined ? l.minzoom : minZoom;
-      const topPoints = [];
-      const bottomPoints = [];
-      let lastZoom;
-      let lastWidth;
-      let path;
-
-      if (Array.isArray(width)) {
-        const [expressionType, [interpolationType, interpolationBase], [attribute]] = width;
-        if (expressionType === 'interpolate' && interpolationType === 'linear' && attribute === 'zoom') {
-          // TODO look into base for interpolation
-
-          const widthArray = width.slice(3);
-          topPoints.push([
-            xScale(layerMinZoom),
-            yScale(l.id) + (yScale.bandwidth() / 2)
-          ]);
-          for (let i = 0; i < widthArray.length; i += 2) {
-            const zoom = widthArray[i];
-            const width = widthArray[i + 1];
-
-            if (zoom < layerMinZoom) continue;
-
-            topPoints.push([xScale(zoom), yScale(l.id) - (width / 2) + yScale.bandwidth() / 2]);
-            if (width > 0) {
-              bottomPoints.push([xScale(zoom), yScale(l.id) + (width / 2) + yScale.bandwidth() / 2]);
-            }
-          }
-
-          [lastZoom, lastWidth] = widthArray.slice(-2);
-        }
-        else if (interpolationType === 'exponential') {
-          // TODO create scale, sample zoom levels, create points
-        }
-        else {
-          console.log('array but not linear', width);
-        }
-      }
-      else if (width && width.stops) {
-        width.stops.forEach(stop => {
-          const [zoom, width] = stop;
-
-          topPoints.push([xScale(zoom), yScale(l.id) - (width / 2) + yScale.bandwidth() / 2]);
-          if (width > 0) {
-            bottomPoints.push([xScale(zoom), yScale(l.id) + (width / 2) + yScale.bandwidth() / 2]);
-          }
-        });
-
-        [lastZoom, lastWidth] = width.stops.slice(-1)[0];
-      }
-      else if (!width) {
-        let layerWidth = 1;
-        topPoints.push([xScale(layerMinZoom), yScale(l.id) - (layerWidth / 2) + yScale.bandwidth() / 2]);
-        topPoints.push([xScale(layerMaxZoom), yScale(l.id) - (layerWidth / 2) + yScale.bandwidth() / 2]);
-        bottomPoints.push([xScale(layerMinZoom), yScale(l.id) + (layerWidth / 2) + yScale.bandwidth() / 2]);
-        bottomPoints.push([xScale(layerMaxZoom), yScale(l.id) + (layerWidth / 2) + yScale.bandwidth() / 2]);
-      }
-
-      if (topPoints.length) {
-        if (layerMaxZoom > lastZoom) {
-          topPoints.push([xScale(layerMaxZoom), yScale(l.id) - (lastWidth / 2) + yScale.bandwidth() / 2]);
-          if (lastWidth > 0) {
-            bottomPoints.push([xScale(layerMaxZoom), yScale(l.id) + (lastWidth / 2) + yScale.bandwidth() / 2]);
-          }
-        }
-
-        path = topPoints.concat(bottomPoints.reverse());
-      }
-
-      return {
-        ...l,
-        path: d3.line()(path || []),
-        fill: color,
-        stroke: strokeColor,
-        strokeWidth: strokeWidth,
-      };
-    });
+    layers = lineLayers.map(l => getDrawLayer(l, xScale, yScale));
   }
 
   onMount(() => {
@@ -259,10 +270,6 @@
         />
       {/each}
     </g>
-
-        chartSvg.append('g')
-            .attr('fill', d => d.fill)
-            .attr('stroke-width', d => d.strokeWidth)
 
     <g transform="translate(0, {margin.top + scrollY})" class="x-axis">
       {#each zoomLevels as zoomLevel} 
