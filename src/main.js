f1861ce53393c411b0af90240ef1aab2d595cbe7
Eric Brelsford <ebrelsford@gmail.com>
2021-12-16 11:40:30 -0500

    Stateful URLs and default style in development

--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import App from './App.svelte';
 
 const app = new App({
 	target: document.body,
-	props: {}
+	props: {
+		loadDefaultStyle: process.env.NODE_ENV === 'development',
+	}
 });
 
 export default app;
