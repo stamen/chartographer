b98b893730359b7eaf1a2477bc783e2c49643ec1
Alex Parlato <alexparlato@stamen.com>
2022-05-24 11:34:48 -0400

    step handling (#32)

--- src/interpolation/step.js
+++ src/interpolation/step.js
@@ -3,8 +3,8 @@ export function getStepFunction(expression) {
     const steps = expression.slice(2).reverse();
     for (let i = 0; i < steps.length; i++) {
       const currentValue = steps[i];
-      const currentZoom = steps[i + 1];
-      if (currentZoom && zoom >= currentZoom) return currentValue;
+      const currentZoom = steps[i + 1] || 0;
+      if (zoom >= currentZoom) return currentValue;
     }
     return steps[0];
   };
